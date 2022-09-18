0dc909af662c005a4e8b6a38650a51cc2599c752
Advait A. Kisar <85425955+AdvaitKisar@users.noreply.github.com>
2022-09-18 13:39:28 +0530

    Update func.py

--- func.py
+++ func.py
@@ -8,7 +8,7 @@ import os
 
 hotel_name = 'Southern Crest' # Name of hotel
 # Path
-path = 'D:/Advait/Python/Personal Projects/Hotel Booking Management System/Customer Data of Hotel Southern Crest.xlsx'
+path = 'D:/Advait/Python/Personal Projects/Hotel Booking Management System/Customer Data of Hotel Southern Crest.xlsx'  # Path needs to be correct as per the location of excelsheet in your system
 
 # Check whether a path pointing to a file
 isFile = os.path.isfile(path)
